d76d3f835d283c162f7e6c06b77d273af080cfce
Andy <warmtree1699@gmail.com>
2022-06-09 22:34:59 +0800

    新增最新商品產品頁

--- app/assets/js/backtop.js
+++ app/assets/js/backtop.js
@@ -1,6 +1,6 @@
 $(function () {
     $('#back_top').click(function () {
-        $('html,body').animate({ scrollTop: 0 }, 333);   /* 返回到最頂上 */
+        $('html,body').animate({ scrollTop: 0 }, 1);   /* 返回到最頂上 */
         return false;
     });
 
