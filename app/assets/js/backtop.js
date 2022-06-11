a4b817cc2bd19c272a7cba331e08f5c5613663f1
Andy <warmtree1699@gmail.com>
2022-06-11 22:26:49 +0800

    Update 2022-06-11T14:26:44.530Z

--- app/assets/js/backtop.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#back_top').click(function () {
-        $('html,body').animate({ scrollTop: 0 }, 1);   /* 返回到最頂上 */
-        return false;
-    });
-
-});
-
---
+++ assets/js/all.js
@@ -0,0 +1,16 @@
+"use strict";
+
+AOS.init();
+"use strict";
+
+$(function () {
+  $('#back_top').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1);
+    /* 返回到最頂上 */
+
+    return false;
+  });
+});
+//# sourceMappingURL=all.js.map
